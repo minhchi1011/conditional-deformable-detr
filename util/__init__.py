aa4f7b95c5e8a52c1e036bf98dbf1b7e015d4c33
Hoang Minh Chi <155606014+minhchi1011@users.noreply.github.com>
2025-06-17 04:02:06 +0700

    fix utils

--- util/__init__.py
+++ util/__init__.py
@@ -1,9 +1,8 @@
 # ------------------------------------------------------------------------
-# Conditional DETR
-# Copyright (c) 2021 Microsoft. All Rights Reserved.
+# Deformable DETR
+# Copyright (c) 2020 SenseTime. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 [see LICENSE for details]
 # ------------------------------------------------------------------------
-# Copied from DETR (https://github.com/facebookresearch/detr)
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# Modified from DETR (https://github.com/facebookresearch/detr)
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 # ------------------------------------------------------------------------
-
