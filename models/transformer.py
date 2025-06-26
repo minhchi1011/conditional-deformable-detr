06e0ecca72a85923b4598a10806d2c4c0bf8b3db
Hoang Minh Chi <155606014+minhchi1011@users.noreply.github.com>
2025-06-27 00:01:15 +0700

    fix the parameter

--- models/transformer.py
+++ models/transformer.py
@@ -62,6 +62,8 @@ def gen_sineembed_for_position(pos_tensor):
     scale = 2 * math.pi
     dim_t = torch.arange(128, dtype=torch.float32, device=pos_tensor.device)
     dim_t = 10000 ** (2 * (dim_t // 2) / 128)
+    dim_t = dim_t.unsqueeze(0).unsqueeze(0)
+    
     x_embed = pos_tensor[:, :, 0] * scale
     y_embed = pos_tensor[:, :, 1] * scale
     pos_x = x_embed[:, :, None] / dim_t
