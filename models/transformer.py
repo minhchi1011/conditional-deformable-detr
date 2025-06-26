e626268c24aa9cffb522a22926c88023b3c3e11a
Hoang Minh Chi <155606014+minhchi1011@users.noreply.github.com>
2025-06-27 00:08:31 +0700

    fix ops

--- models/transformer.py
+++ models/transformer.py
@@ -60,10 +60,16 @@ def gen_sineembed_for_position(pos_tensor):
     # n_query, bs, _ = pos_tensor.size()
     # sineembed_tensor = torch.zeros(n_query, bs, 256)
     scale = 2 * math.pi
+    if pos_tensor.dim() == 4:
+        bs, num_queries, num_levels, _ = pos_tensor.shape
+        pos_tensor = pos_tensor.view(bs, num_queries * num_levels, -1)
+        need_reshape_back = True
+    else:
+        need_reshape_back = False
     dim_t = torch.arange(128, dtype=torch.float32, device=pos_tensor.device)
     dim_t = 10000 ** (2 * (dim_t // 2) / 128)
     dim_t = dim_t.unsqueeze(0).unsqueeze(0)
-    
+
     x_embed = pos_tensor[:, :, 0] * scale
     y_embed = pos_tensor[:, :, 1] * scale
     pos_x = x_embed[:, :, None] / dim_t
@@ -71,6 +77,8 @@ def gen_sineembed_for_position(pos_tensor):
     pos_x = torch.stack((pos_x[:, :, 0::2].sin(), pos_x[:, :, 1::2].cos()), dim=3).flatten(2)
     pos_y = torch.stack((pos_y[:, :, 0::2].sin(), pos_y[:, :, 1::2].cos()), dim=3).flatten(2)
     pos = torch.cat((pos_y, pos_x), dim=2)
+    if need_reshape_back:
+        pos = pos.view(bs, num_queries, num_levels, -1)
     return pos
 
 class Transformer(nn.Module):
